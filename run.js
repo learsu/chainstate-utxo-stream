0220ef25768e08815e7bcf03afe2c46e079e9088
Stanislas Marion <stanislas.marion@gmail.com>
2016-02-06 17:04:33 +0100

    cleaning

--- run.js
+++ run.js
@@ -5,18 +5,10 @@ var utxoStream = require('./')
 
 var db = level('./chainstate', { keyEncoding: 'hex', valueEncoding: 'hex' })
 var rs = db.createReadStream({ gte: '63', lt: '64' })
-console.time('parse-utxo')
-// rs.on('data', function (data) {
-  // console.log(data)
-// })
 rs.pipe(utxoStream())
   .pipe(through.obj(function (chunk, enc, done) {
     chunk.forEach(function (txOut) {
       txOut.txHash = txOut.txHash.toString('hex')
-      // console.log(txOut)
     })
     done()
   }))
-  .on('end', function () {
-    console.timeEnd('parse-utxo')
-  })
